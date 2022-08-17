0a1bd172096512f2fbaf75d92e03e719a93a1acd
Nguyễn An Vĩ <nguyenanvi122333@gmail.com>
2022-08-17 23:39:45 +0700

    11:40 17/8/22

--- resources/js/bootstrap.js
+++
@@ -1,9 +0,0 @@
-window._ = require('lodash');
-
-window.UIkit = require("../../vendor/uikit/uikit");
-window.Icons = require("../../vendor/uikit/uikit/dist/js/uikit-icons");
-
-UIkit.use(window.Icons);
-
-window.axios = require('axios');
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -120,5 +120,6 @@
     </footer>
 </div>
 <script src="{{ asset('js/app.js') }}" defer></script>
+<script src="{{ asset('js/uikit-icons.js') }}" defer></script>
 </body>
 </html>
