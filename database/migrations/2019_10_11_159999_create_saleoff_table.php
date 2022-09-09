5817f41bb74f224825f08801a79d4db8d34ef104
Nguyễn An Vĩ <nguyenanvi122333@gmail.com>
2022-09-09 15:34:21 +0700

    migrate=>Products and saleoff table

---
+++ database/migrations/2019_10_11_159999_create_saleoff_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSaleoffTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('saleoff', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('price');
+            $table->timestamp('expire');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('saleoff');
+    }
+}
---
+++ database/migrations/2019_10_11_160000_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('image');
+            // $table->string('email')->unique();
+            $table->text('detail');
+            $table->integer('price');
+            $table->unsignedBigInteger('saleoff_id')->nullable();
+            $table->foreign('saleoff_id')->references('id')->on('saleoff')->nullOnDelete();
+            // $table->foreignId('saleoff_id')->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
