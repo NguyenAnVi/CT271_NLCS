6b5ec639f870b9d9793741ff4be22bf2deefedbb
Nguyen An Vi <nguyenanvi122333gmail.com>
2022-10-02 00:11:53 +0700

    improve function getImageAt

--- app/Helpers.php
+++ app/Helpers.php
@@ -3,6 +3,8 @@
 if (!function_exists('getImages')) {
     function getImageAt($array, $position)
     {
+        if($array)
         return json_decode(str_replace('\\','',$array))[$position];
+        return NULL;
     }
 }
